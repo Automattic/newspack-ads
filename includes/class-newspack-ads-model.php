6ff6d1fd407ee08fa99795699c652cd3b0788b35
Jefferson Rabb <jeff@atavist.net>
2020-01-09 14:00:49 -0500

    Support for multiple dimensions in amp-ad rendering.

--- includes/class-newspack-ads-model.php
+++ includes/class-newspack-ads-model.php
@@ -306,18 +306,45 @@ class Newspack_Ads_Model {
 	 * @param array $ad_unit The ad unit to generate AMP code for.
 	 */
 	public static function amp_code_for_ad_unit( $ad_unit ) {
-		$width        = $ad_unit->width;
-		$height       = $ad_unit->height;
+		$sizes        = $ad_unit->sizes;
 		$name         = $ad_unit->post_title;
 		$network_code = self::get_network_code( 'google_ad_manager' );
 
-		$code = sprintf(
-			'<amp-ad width=%s height=%s type="doubleclick" data-slot="/%s/%s"></amp-ad>',
-			$width,
-			$height,
-			$network_code,
-			$name
+		$largest = array_reduce(
+			$sizes,
+			function( $carry, $item ) {
+				return $item[0] > $carry[0] ? $item : $carry;
+			},
+			[ 0, 0 ]
 		);
+
+		$other_sizes = array_filter(
+			$sizes,
+			function( $item ) use ( $largest ) {
+				return $item !== $largest;
+			}
+		);
+
+		$data_multi_size = '';
+		if ( count( $other_sizes ) ) {
+			$formatted_sizes = array_map(
+				function( $item ) {
+					return $item[0] . 'x' . $item[1];
+				},
+				$other_sizes
+			);
+			$data_multi_size = sprintf( 'data-multi-size="%s"', implode( ',', $formatted_sizes ) );
+		}
+
+		$code = sprintf(
+			'<amp-ad width=%s height=%s type="doubleclick" data-slot="/%s/%s" %s></amp-ad>',
+			$largest[0],
+			$largest[1],
+			$network_code,
+			$name,
+			$data_multi_size
+		);
+
 		return $code;
 	}
 }
