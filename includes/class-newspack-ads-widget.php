882ccadae2a39f7e00c3c1224a0500cb02f82985
claudiulodro <claudiulodro@gmail.com>
2020-03-11 11:53:29 -0700

    chore: couple small code tweaks

--- includes/class-newspack-ads-widget.php
+++ includes/class-newspack-ads-widget.php
@@ -43,10 +43,14 @@ class Newspack_Ads_Widget extends WP_Widget {
 	 * @param object $instance The Widget instance.
 	 */
 	public function widget( $args, $instance ) {
+		if ( ! newspack_should_show_ads() ) {
+			return;
+		}
+
 		$selected_ad_unit = $instance['selected_ad_unit'];
 		$ad_unit          = Newspack_Ads_Model::get_ad_unit( $selected_ad_unit );
 
-		if ( is_wp_error( $ad_unit ) || ! newspack_should_show_ads() ) {
+		if ( is_wp_error( $ad_unit ) ) {
 			return;
 		}
 
