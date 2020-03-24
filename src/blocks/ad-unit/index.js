b43819fcee99a7fdff634cda85b9eac24b277d01
Adam Boro <adam@adamboro.com>
2020-03-24 17:55:28 +0100

    ci: set up ci, add semantic-release, fix linting errors (#32)
    - ci(release): set up semantic-release and linting
    - fix: linting errors
    - docs: add badges to readme
    - chore: update composer.lock
    - chore: use lint-staged for pre-commit js & php linting
    - fix: php linting errors

--- src/blocks/ad-unit/index.js
+++ src/blocks/ad-unit/index.js
@@ -22,7 +22,7 @@ export const title = __( 'Ad Unit' );
 /* From https://material.io/tools/icons */
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-9 9H3V5h9v7z"/>
+		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-9 9H3V5h9v7z" />
 	</SVG>
 );
 export const settings = {
@@ -33,21 +33,19 @@ export const settings = {
 	description: (
 		<Fragment>
 			<p>{ __( 'A block for displaying ad inventory.', 'newspack' ) }</p>
-			<ExternalLink href="/wp-admin/admin.php?page=newspack-advertising-wizard#/google_ad_manager">{ __( 'Manage ad units', 'newspack' ) }</ExternalLink>
+			<ExternalLink href="/wp-admin/admin.php?page=newspack-advertising-wizard#/google_ad_manager">
+				{ __( 'Manage ad units', 'newspack' ) }
+			</ExternalLink>
 		</Fragment>
 	),
 	attributes: {
 		activeAd: {
 			type: 'string',
-		}
+		},
 	},
 	supports: {
 		html: false,
-		align: [
-			'left',
-			'center',
-			'right',
-		],
+		align: [ 'left', 'center', 'right' ],
 	},
 	edit,
 	save: () => null, // to use view.php
