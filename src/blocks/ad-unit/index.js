152e53f3f94f27915e2ba6609bd832d297318149
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-12-04 13:32:11 +0000

    feat: add Gutenberg icon and replace Material

--- src/blocks/ad-unit/index.js
+++ src/blocks/ad-unit/index.js
@@ -1,10 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { ExternalLink, Path, SVG } from '@wordpress/components';
+import { ExternalLink } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 import { getCategories } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { Icon, stretchWide } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -19,19 +20,12 @@ import './editor.scss';
 export const name = 'ad-unit';
 export const title = __( 'Ad Unit' );
 
-/* From https://material.io/tools/icons */
-export const icon = (
-	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-		<Path d="M0 0h24v24H0z" fill="none" />
-		<Path
-			d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM4 6h9v7H4z"
-			fill="#36f"
-		/>
-	</SVG>
-);
 export const settings = {
 	title,
-	icon,
+	icon: {
+		src: <Icon icon={ stretchWide } />,
+		foreground: '#36f',
+	},
 	category: getCategories().some( ( { slug } ) => slug === 'newspack' ) ? 'newspack' : 'common',
 	keywords: [ __( 'ad' ), __( 'advert' ), __( 'ads' ) ],
 	description: (
