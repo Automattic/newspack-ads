6c57c71aa3ab7ce20daafadc3305bc4bbed38a1e
matticbot <semantic-release-bot@martynus.net>
2021-01-28 23:00:37 +0000

    chore(release): 1.11.1-alpha.1 [skip ci]
    ## [1.11.1-alpha.1](https://github.com/Automattic/newspack-ads/compare/v1.11.0...v1.11.1-alpha.1) (2021-01-28)
    
    ### Bug Fixes
    
    * for sticky ads, use smallest ad unit size available ([#101](https://github.com/Automattic/newspack-ads/issues/101)) ([8b4da54](https://github.com/Automattic/newspack-ads/commit/8b4da54235c0fa15fad3d8ef67791a332f65a5be))

--- newspack-ads.php
+++ newspack-ads.php
@@ -5,7 +5,7 @@
  * Description:     Ad services integration.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.11.0
+ * Version:         1.11.1-alpha.1
  *
  * @package         Newspack
  */
