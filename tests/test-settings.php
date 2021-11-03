5bacce58fd3a8ebccd66dd80299ba0473d992242
Miguel Peixe <miguel@peixe.co>
2021-11-03 12:10:58 -0300

    test: get public and update values

---
+++ tests/test-settings.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Tests the ads settings functionality.
+ *
+ * @package Newspack\Tests
+ */
+
+/**
+ * Test ads settings functionality.
+ */
+class SettingsTest extends WP_UnitTestCase {
+	/**
+	 * Sample settings list for tests
+	 *
+	 * @var array
+	 */
+	private static $settings_list = [
+		[
+			'description' => 'A setting section',
+			'help'        => 'A setting section description or help text',
+			'section'     => 'test_section',
+			'key'         => 'active',
+			'type'        => 'boolean',
+			'default'     => false,
+			'public'      => true,
+		],
+		[
+			'description' => 'My first field',
+			'help'        => 'Help text',
+			'section'     => 'test_section',
+			'key'         => 'first_field',
+			'type'        => 'boolean',
+			'default'     => true,
+			'public'      => true,
+		],
+		[
+			'description' => 'My number field',
+			'help'        => 'Help text',
+			'section'     => 'test_section',
+			'key'         => 'number_field',
+			'type'        => 'integer',
+			'default'     => true,
+			'public'      => true,
+		],
+		[
+			'description' => 'A private field',
+			'help'        => 'Help text',
+			'section'     => 'test_section',
+			'key'         => 'private_field',
+			'type'        => 'string',
+			'default'     => '',
+			'public'      => false,
+		],
+	];
+
+	/**
+	 * Set up stuff for testing.
+	 */
+	public function setUp() {
+		parent::setUp();
+		add_filter( 'newspack_ads_settings_list', [ __CLASS__, 'set_settings_list' ] );
+	}
+
+	/**
+	 * Add sample settings list.
+	 * 
+	 * @param array $settings_list List of settings.
+	 * 
+	 * @return array Updated settings list.
+	 */
+	public static function set_settings_list( $settings_list ) {
+		return $settings_list + self::$settings_list;
+	}
+  
+	/**
+	 * Test that the returned settings values does not contain non-public values.
+	 */
+	public function test_get_public_settings() {
+		$settings = Newspack_Ads_Settings::get_settings( 'test_section', true );
+		self::assertFalse(
+			isset( $settings['private_field'] ),
+			'Private settings should not be returned'
+		);
+	}
+
+	/**
+	 * Test that the values are properly updated with configured type.
+	 */
+	public function test_update_value() {
+		Newspack_Ads_Settings::update_section(
+			'test_section',
+			[
+				'active'        => '1',
+				'first_field'   => '0',
+				'number_field'  => '200',
+				'private_field' => true,
+			]
+		);
+		$settings = Newspack_Ads_Settings::get_settings( 'test_section' );
+		self::assertSame(
+			$settings['active'],
+			true,
+			'Boolean value should be updated with proper type'
+		);
+		self::assertSame(
+			$settings['first_field'],
+			false,
+			'Boolean value should be updated with proper type'
+		);
+		self::assertSame(
+			$settings['number_field'],
+			200,
+			'Integer value should be updated with proper type'
+		);
+		self::assertSame(
+			$settings['private_field'],
+			'1',
+			'String value should be updated with proper type'
+		);
+	}
+}
