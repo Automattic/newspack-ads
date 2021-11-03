0da85b362cf8a55d4402ffa35532a333c7fc43cb
matticbot <semantic-release-bot@martynus.net>
2021-11-03 16:00:46 +0000

    chore(release): 1.18.1 [skip ci]
    ## [1.18.1](https://github.com/Automattic/newspack-ads/compare/v1.18.0...v1.18.1) (2021-11-03)
    
    ### Bug Fixes
    
    * update circleci build images ([#214](https://github.com/Automattic/newspack-ads/issues/214)) ([78da65e](https://github.com/Automattic/newspack-ads/commit/78da65e9234d0c3c64ae5b148e213cb313ccf57d))

--- newspack-ads.php
+++ newspack-ads.php
@@ -5,7 +5,7 @@
  * Description:     Ad services integration.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.18.0
+ * Version:         1.18.1
  *
  * @package         Newspack
  */
