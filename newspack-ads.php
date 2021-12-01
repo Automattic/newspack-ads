d36de18a06997cc078db52491206960e49d47261
matticbot <semantic-release-bot@martynus.net>
2021-12-01 16:24:30 +0000

    chore(release): 1.22.0 [skip ci]
    # [1.22.0](https://github.com/Automattic/newspack-ads/compare/v1.21.0...v1.22.0) (2021-12-01)
    
    ### Features
    
    * **scaip:** opt-out of integrated placements ([#246](https://github.com/Automattic/newspack-ads/issues/246)) ([7069973](https://github.com/Automattic/newspack-ads/commit/7069973e7781cf120d7ef1f98169cd37549f2aed))

--- newspack-ads.php
+++ newspack-ads.php
@@ -5,7 +5,7 @@
  * Description:     Ad services integration.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.22.0-alpha.1
+ * Version:         1.22.0
  *
  * @package         Newspack
  */
