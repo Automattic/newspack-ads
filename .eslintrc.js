a2f2369ca4c0b92a83f2499b870123499c6ff726
Derrick Koo <derrick.koo@automattic.com>
2022-01-24 13:53:41 -0700

    chore: use newspack-scripts, but not for build or eslint commands

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,11 @@ module.exports = {
 	env: {
 		browser: true,
 	},
+	ignorePatterns: [ 'dist/', 'node_modules/', 'postcss.config.js' ],
 	rules: {
 		// Disallow importing or requiring packages that are not listed in package.json
 		// This prevents us from depending on transitive dependencies, which could break in unexpected ways.
-		'import/no-extraneous-dependencies': ['error'],
+		'import/no-extraneous-dependencies': [ 'error' ],
 		// Skip prop types validation for now
 		'react/prop-types': 'off',
 		// Allow exporting components as default exports
