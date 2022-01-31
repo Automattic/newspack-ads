645f4793409936443b0bc1a12983d9e8faf47cce
matticbot <sysops+ghmatticbot@automattic.com>
2022-01-31 18:42:33 +0000

    chore(release): merge in release v1.26.0

--- newspack-ads.php
+++ newspack-ads.php
@@ -5,7 +5,7 @@
  * Description:     Ad services integration.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.25.0
+ * Version:         1.26.0
  *
  * @package         Newspack
  */
