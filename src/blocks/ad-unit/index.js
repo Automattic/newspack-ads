5919ddd145b8b3b8c0f8c6a20116789c3821765d
Thomas Guillot <thomasguillot@users.noreply.github.com>
2022-03-15 10:00:05 +0000

    feat: update block icon
    for simplicity, use pullquote instead of a custom one

--- src/blocks/ad-unit/index.js
+++ src/blocks/ad-unit/index.js
@@ -1,8 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { Path, SVG } from '@wordpress/components';
 import { getCategories } from '@wordpress/blocks';
+import { pullquote } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -16,26 +16,20 @@ import edit from './edit';
 import './editor.scss';
 
 export const name = 'ad-unit';
-export const title = __( 'Ad Unit' );
-export const icon = (
-	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-		<Path d="M7 5.5h10V4H7zM17 20H7v-1.5h10zM17.75 9.75l-11.5 4.5v-4.5z" />
-		<Path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M20 9.5a2 2 0 00-2-2H6a2 2 0 00-2 2v5a2 2 0 002 2h12a2 2 0 002-2zM18 9H6a.5.5 0 00-.5.5v5a.5.5 0 00.5.5h12a.5.5 0 00.5-.5v-5A.5.5 0 0018 9z"
-		/>
-	</SVG>
-);
+export const title = __( 'Ad Unit', 'newspack-ads' );
 
 export const settings = {
 	title,
 	icon: {
-		src: icon,
+		src: pullquote,
 		foreground: '#36f',
 	},
 	category: getCategories().some( ( { slug } ) => slug === 'newspack' ) ? 'newspack' : 'common',
-	keywords: [ __( 'ad' ), __( 'advert' ), __( 'ads' ) ],
+	keywords: [
+		__( 'ad', 'newspack-ads' ),
+		__( 'advert', 'newspack-ads' ),
+		__( 'ads', 'newspack-ads' ),
+	],
 	description: __( 'Render an ad unit from your inventory.', 'newspack-ads' ),
 	attributes: {
 		// Legacy attribute.
