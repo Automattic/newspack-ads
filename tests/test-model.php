4b012d1274d7891913fafa8ae74e2e554a161680
Miguel Peixe <miguel@peixe.co>
2022-04-12 13:06:17 -0300

    feat(gam): size map filters and width threshold (#366)

--- tests/test-model.php
+++ tests/test-model.php
@@ -218,7 +218,30 @@ class ModelTest extends WP_UnitTestCase {
 				'90'  => [ [ 60, 60 ], [ 90, 90 ] ],
 				'100' => [ [ 60, 60 ], [ 90, 90 ], [ 100, 100 ] ],
 			],
-			GAM_Model::get_responsive_size_map( [ [ 10, 10 ], [ 100, 100 ], [ 90, 90 ], [ 60, 60 ] ], 0.5 )
+			GAM_Model::get_responsive_size_map( [ [ 10, 10 ], [ 100, 100 ], [ 90, 90 ], [ 60, 60 ] ], 0.5 ),
+			'Groups sizes with a custom difference ratio.'
+		);
+
+		self::assertEquals(
+			[
+				'300' => [ [ 300, 200 ], [ 300, 250 ] ],
+				'350' => [ [ 350, 200 ] ],
+				'640' => [ [ 640, 360 ] ],
+				'960' => [ [ 640, 360 ], [ 960, 540 ] ],
+			],
+			GAM_Model::get_responsive_size_map( [ [ 300, 200 ], [ 300, 250 ], [ 350, 200 ], [ 640, 360 ], [ 960, 540 ] ], 0 ),
+			'Groups sizes above the default width threshold of 600 regardless of their ratio difference.'
+		);
+
+		self::assertEquals(
+			[
+				'300' => [ [ 300, 200 ], [ 300, 250 ] ],
+				'350' => [ [ 350, 200 ] ],
+				'640' => [ [ 640, 360 ] ],
+				'960' => [ [ 960, 540 ] ],
+			],
+			GAM_Model::get_responsive_size_map( [ [ 300, 200 ], [ 300, 250 ], [ 350, 200 ], [ 640, 360 ], [ 960, 540 ] ], 0, false ),
+			'Groups sizes without ratio difference and threshold disabled.'
 		);
 	}
 }
