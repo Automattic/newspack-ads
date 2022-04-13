f3403dccec86c10e9092706f3553a47e8041f2c1
Miguel Peixe <miguel@peixe.co>
2022-04-13 16:26:58 -0300

    chore(bidding): remove unused methods

--- includes/class-bidding.php
+++ includes/class-bidding.php
@@ -233,28 +233,6 @@ final class Bidding {
 	}
 
 	/**
-	 * Return a string from a size array.
-	 *
-	 * @param array $size Size array.
-	 *
-	 * @return string Size string.
-	 */
-	private static function get_size_string( $size ) {
-		return $size[0] . 'x' . $size[1];
-	}
-
-	/**
-	 * Return an array from a size string.
-	 *
-	 * @param string $size Size string.
-	 *
-	 * @return array Size array.
-	 */
-	private static function get_size_array( $size ) {
-		return array_map( 'intval', explode( 'x', $size ) );
-	}
-
-	/**
 	 * Sanitize an array of price buckets.
 	 *
 	 * @param array[] $price_buckets Array of price buckets.
