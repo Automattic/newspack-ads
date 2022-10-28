e82c57b3568d8e3e32879a047fc63f6ad50d051c
Miguel Peixe <miguel.peixe@a8c.com>
2022-10-28 18:22:49 -0300

    feat: product crud with wc

--- includes/marketplace/class-marketplace.php
+++ includes/marketplace/class-marketplace.php
@@ -8,7 +8,7 @@
 namespace Newspack_Ads;
 
 use Newspack_Ads\Settings;
-use Newspack_Ads\Marketplace\Ad_Product;
+use WC_Product_Simple;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -19,75 +19,90 @@ final class Marketplace {
 
 	const PRODUCTS_OPTION_NAME = '_newspack_ads_products';
 
+	const PRODUCT_META_PREFIX = '_ad_';
+
 	/**
 	 * Initialize hooks.
 	 */
 	public static function init() {
-		add_action( 'init', [ __CLASS__, 'register_product_type' ] );
-		add_action( 'rest_api_init', [ __CLASS__, 'register_rest_routes' ] );
-		add_filter( 'product_type_selector', [ __CLASS__, 'add_product_type' ] );
-		add_filter( 'woocommerce_product_data_tabs', [ __CLASS__, 'product_tab' ] );
-		add_action( 'woocommerce_product_data_panels', [ __CLASS__, 'product_panel' ] );
-		add_action( 'woocommerce_process_product_meta', [ __CLASS__, 'save_product_settings' ] );
+		\add_action( 'rest_api_init', [ __CLASS__, 'register_rest_routes' ] );
 	}
 
 	/**
 	 * Register API endpoints.
 	 */
 	public static function register_rest_routes() {
-		register_rest_route(
+		\register_rest_route(
 			Settings::API_NAMESPACE,
-			'/ad-product/(?P<placement>.+)',
+			'/products',
 			[
 				'methods'             => \WP_REST_Server::READABLE,
 				'callback'            => [ __CLASS__, 'api_get' ],
 				'permission_callback' => [ 'Newspack_Ads\Settings', 'api_permissions_check' ],
 			]
 		);
-		register_rest_route(
+		\register_rest_route(
 			Settings::API_NAMESPACE,
-			'/ad-product/(?P<placement>.+)',
+			'/products/(?P<placement>.+)',
+			[
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => [ __CLASS__, 'api_get' ],
+				'permission_callback' => [ 'Newspack_Ads\Settings', 'api_permissions_check' ],
+			]
+		);
+		\register_rest_route(
+			Settings::API_NAMESPACE,
+			'/products/(?P<placement>.+)',
 			[
 				'methods'             => \WP_REST_Server::EDITABLE,
 				'callback'            => [ __CLASS__, 'api_update' ],
 				'permission_callback' => [ 'Newspack_Ads\Settings', 'api_permissions_check' ],
-				'args'                => [
-					'ad_unit'     => [
-						'required'          => true,
-						'type'              => 'string',
-						'sanitize_callback' => 'sanitize_text_field',
-					],
-					'prices'      => [
-						'required'          => true,
-						'type'              => 'object',
-						'sanitize_callback' => [ __CLASS__, 'sanitize_prices' ],
-					],
-					'is_per_size' => [
-						'type'              => 'boolean',
-						'sanitize_callback' => 'rest_sanitize_boolean',
-						'default'           => false,
-					],
-					'per_size'    => [
-						'type'              => 'object',
-						'sanitize_callback' => [ __CLASS__, 'sanitize_per_size' ],
-					],
-					'event'       => [
-						'required'          => true,
-						'type'              => 'string',
-						'sanitize_callback' => 'sanitize_text_field',
-					],
-				],
+				'args'                => self::get_ad_product_args(),
 			]
 		);
-		register_rest_route(
+		\register_rest_route(
 			Settings::API_NAMESPACE,
-			'/ad-product/(?P<placement>.+)',
+			'/products/(?P<placement>.+)',
 			[
 				'methods'             => \WP_REST_Server::DELETABLE,
 				'callback'            => [ __CLASS__, 'api_delete' ],
 				'permission_callback' => [ 'Newspack_Ads\Settings', 'api_permissions_check' ],
 			]
 		);
+	}
+
+	/**
+	 * Ad Product REST Arguments
+	 *
+	 * @return array
+	 */
+	private static function get_ad_product_args() {
+		return [
+			'ad_unit'     => [
+				'required'          => true,
+				'type'              => 'string',
+				'sanitize_callback' => 'sanitize_text_field',
+			],
+			'prices'      => [
+				'required'          => true,
+				'type'              => 'object',
+				'sanitize_callback' => [ __CLASS__, 'sanitize_prices' ],
+			],
+			'is_per_size' => [
+				'type'              => 'boolean',
+				'sanitize_callback' => 'rest_sanitize_boolean',
+				'default'           => false,
+			],
+			'per_size'    => [
+				'type'              => 'object',
+				'sanitize_callback' => [ __CLASS__, 'sanitize_per_size' ],
+			],
+			'event'       => [
+				'required'          => true,
+				'type'              => 'string',
+				'sanitize_callback' => 'sanitize_text_field',
+			],
+		];
 	}
 
 	/**
@@ -138,8 +153,21 @@ final class Marketplace {
 
 	/**
 	 * Get a product by placement.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
 	 */
-	public static function api_get() {}
+	public static function api_get( $request ) {
+		$products = self::get_products();
+		if ( ! empty( $request['placement'] ) ) {
+			$placement = $request['placement'];
+			if ( ! empty( $products[ $placement ] ) ) {
+				return \rest_ensure_response( self::get_product_data( $products[ $placement ] ) );
+			} else {
+				return new \WP_Error( 'newspack_ads_product_not_found', __( 'Ad product not found.', 'newspack-ads' ), [ 'status' => 404 ] );
+			}
+		}
+		return \rest_ensure_response( array_map( [ __CLASS__, 'get_product_data' ], $products ) );
+	}
 
 	/**
 	 * Create a new ad product.
@@ -149,23 +177,89 @@ final class Marketplace {
 	 * @return WP_REST_Response containing the settings list.
 	 */
 	public static function api_update( $request ) {
-		$params  = $request->get_params();
-		$product = self::get_product( $params['placement'] );
+		$args      = array_intersect_key( $request->get_params(), self::get_ad_product_args() );
+		$placement = $request['placement'];
+		$product   = self::get_product( $placement );
 		if ( ! $product ) {
-			$product = new Ad_Product( $params['placement'] );
+			$product = new WC_Product_Simple();
 		}
-		$payable_events = self::get_payable_events();
-		$price          = $params['prices'][ $payable_events[ $params['event'] ] ];
-		$product->set_price( $price );
-		$product->save();
-		self::set_placement_product( $product );
-		return \rest_ensure_response( $product->get_data() );
+		$product = self::update_product( $placement, $product, $args );
+		return \rest_ensure_response( self::get_product_data( $product ) );
 	}
 
 	/**
 	 * Delete an ad product.
 	 */
 	public static function api_delete() {}
+
+	/**
+	 * Update a product with the sanitized arguments.
+	 *
+	 * @param string            $placement The ad placement.
+	 * @param WC_Product_Simple $product   The product to update.
+	 * @param array             $args      The sanitized ad product arguments.
+	 *
+	 * @return WC_Product_Simple The updated product.
+	 */
+	private static function update_product( $placement, $product, $args ) {
+		$payable_events = self::get_payable_events();
+		$price          = $args['prices'][ $payable_events[ $args['event'] ] ];
+		$product->set_name( __( 'Ad Placement', 'newspack-ads' ) . ' - ' . $placement );
+		$product->set_virtual( true );
+		$product->is_visible( false );
+		$product->set_regular_price( $price );
+		$product->save();
+		self::set_product_meta( $product->get_id(), 'placement', $placement );
+		foreach ( $args as $key => $value ) {
+			self::set_product_meta( $product->get_id(), $key, $value );
+		}
+		self::set_placement_product( $placement, $product );
+		return $product;
+	}
+
+	/**
+	 * Set the placement product.
+	 *
+	 * @param string            $placement The ad placement.
+	 * @param WC_Product_Simple $product   The product to set.
+	 *
+	 * @return bool Whether the value was updated or not.
+	 */
+	private static function set_placement_product( $placement, $product ) {
+		$id = $product->get_id();
+		/** Bail if WC Product is not saved. */
+		if ( ! $id ) {
+			return;
+		}
+		$products               = self::get_products();
+		$products[ $placement ] = $id;
+		return update_option( self::PRODUCTS_OPTION_NAME, $products );
+	}
+
+	/**
+	 * Set a product meta.
+	 *
+	 * @param int    $product_id The product ID.
+	 * @param string $key        The meta key.
+	 * @param mixed  $value      The meta value.
+	 *
+	 * @return void
+	 */
+	private static function set_product_meta( $product_id, $key, $value ) {
+		\update_post_meta( $product_id, self::PRODUCT_META_PREFIX . $key, $value );
+	}
+
+	/**
+	 * Get a product meta.
+	 *
+	 * @param int    $product_id The product ID.
+	 * @param string $key        The meta key.
+	 *
+	 * @return mixed
+	 */
+	private static function get_product_meta( $product_id, $key ) {
+		return \get_post_meta( $product_id, self::PRODUCT_META_PREFIX . $key, true );
+	}
 
 	/**
 	 * Get ad product payable events and its value unit (e.g. CPM, CPC).
@@ -183,7 +277,7 @@ final class Marketplace {
 	/**
 	 * Get all placement products.
 	 *
-	 * @return Ad_Product[] Ad products keyed by their placement.
+	 * @return WC_Product_Simple[] Ad products keyed by their placement.
 	 */
 	public static function get_products() {
 		$ids = get_option( self::PRODUCTS_OPTION_NAME, [] );
@@ -193,12 +287,12 @@ final class Marketplace {
 		$products = [];
 		foreach ( $ids as $placement => $id ) {
 			try {
-				$product = new Ad_Product( $placement, $id );
+				$product = \wc_get_product( $id );
 			} catch ( \Exception $e ) {
 				continue;
 			}
 			if ( $product && ! is_wp_error( $product ) ) {
-				$products[ $placement ] = $product;
+				$products[ $placement ] = new WC_Product_Simple( $product );
 			}
 		}
 		return $products;
@@ -209,7 +303,7 @@ final class Marketplace {
 	 *
 	 * @param string $placement The product placement.
 	 *
-	 * @return Ad_Product|null Ad product or null if not found.
+	 * @return WC_Product_Simple|null Ad product or null if not found.
 	 */
 	public static function get_product( $placement ) {
 		$products = self::get_products();
@@ -217,94 +311,25 @@ final class Marketplace {
 	}
 
 	/**
-	 * Set the placement product.
+	 * Get ad product data.
 	 *
-	 * @param Ad_Product $product The product to set.
+	 * @param WC_Product_Simple $product The product.
 	 *
-	 * @return bool Whether the value was updated or not.
+	 * @return array
 	 */
-	public static function set_placement_product( $product ) {
-		$id = $product->get_id();
-		if ( ! $id ) {
-			return;
+	public static function get_product_data( $product ) {
+		if ( ! $product || ! $product->get_id() ) {
+			return [];
 		}
-		$products                              = self::get_products();
-		$products[ $product->get_placement() ] = $id;
-		return update_option( self::PRODUCTS_OPTION_NAME, $products );
-	}
-
-	/**
-	 * Register the product type.
-	 */
-	public static function register_product_type() {
-		if ( ! class_exists( 'WC_Product' ) ) {
-			return;
+		$payable_events = self::get_payable_events();
+		$args_keys      = array_keys( self::get_ad_product_args() );
+		$data           = [
+			'id' => $product->get_id(),
+		];
+		foreach ( $args_keys as $key ) {
+			$data[ $key ] = self::get_product_meta( $product->get_id(), $key );
 		}
-		require_once 'class-ad-product.php';
-	}
-
-	/**
-	 * Add Newspack Ad product type.
-	 *
-	 * @param string[] $types Product types.
-	 *
-	 * @return string[] Product types.
-	 */
-	public static function add_product_type( $types ) {
-		$types['newspack-ad'] = __( 'Newspack Ad', 'newspack-ads' );
-		return $types;
-	}
-
-	/**
-	 * Add Newspack Ad product tab.
-	 *
-	 * @param string[] $tabs Product tabs.
-	 *
-	 * @return string[] Product tabs.
-	 */
-	public static function product_tab( $tabs ) {
-		$tabs['newspack-ad'] = array(
-			'label'  => __( 'Newspack Ad', 'newspack-ads' ),
-			'target' => 'newspack_ad_product_options',
-			'class'  => 'show_if_demo_product',
-		);
-		return $tabs;
-	}
-
-	/**
-	 * Register Ad Product Panel.
-	 */
-	public static function product_panel() {
-		?><div id='newspack_ad_product_options' class='panel woocommerce_options_panel'><div class='options_group'>
-		<?php
-		woocommerce_wp_text_input(
-			array(
-				'id'          => 'newspack_ad_product_info',
-				'label'       => __( 'Newspack Ad Product Spec', 'newspack-ads' ),
-				'placeholder' => '',
-				'desc_tip'    => 'true',
-				'description' => __( 'Enter Newspack Ad Product Info.', 'newspack-ads' ),
-				'type'        => 'text',
-			)
-		);
-		?>
-		</div></div>
-		<?php
-	}
-
-	/**
-	 * Save product settings.
-	 *
-	 * @param int $post_id Post ID.
-	 */
-	public static function save_product_settings( $post_id ) {
-		if ( ! isset( $_POST['newspack_ad_product_info'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-			return;
-		}
-		$product_info = sanitize_text_field( $_POST['newspack_ad_product_info'] ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
-		if ( ! empty( $product_info ) ) {
-			update_post_meta( $post_id, 'newspack_ad_product_info', esc_attr( $product_info ) );
-		}
+		return $data;
 	}
 }
 Marketplace::init();
