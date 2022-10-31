06af31000022f1a518b54f6b133977ee9b64917a
matticbot <semantic-release-bot@martynus.net>
2022-10-31 18:37:18 +0000

    chore(release): 1.39.2 [skip ci]
    ## [1.39.2](https://github.com/Automattic/newspack-ads/compare/v1.39.1...v1.39.2) (2022-10-31)
    
    ### Bug Fixes
    
    * trigger release ([5d0fd4b](https://github.com/Automattic/newspack-ads/commit/5d0fd4b904dd6b3c029446c5a1e78406dd30dbed))

--- newspack-ads.php
+++ newspack-ads.php
@@ -5,7 +5,7 @@
  * Description:     Ad services integration.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.39.2-alpha.1
+ * Version:         1.39.2
  *
  * @package         Newspack
  */
