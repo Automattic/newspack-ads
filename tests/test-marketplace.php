eec8dd7cffb4cb0aa375b21b99dd9b14cf60ce22
Miguel Peixe <miguel.peixe@a8c.com>
2022-11-25 13:13:57 -0300

    test: crud

--- tests/test-marketplace.php
+++ tests/test-marketplace.php
@@ -5,6 +5,9 @@
  * @package Newspack\Tests
  */
 
+use Newspack_Ads\Marketplace;
+use Newspack_Ads\Placements;
+
 /**
  * Test Marketplace
  */
@@ -100,7 +103,6 @@ class MarketplaceTest extends WP_UnitTestCase {
 		$this->assertEquals( '5', $data['price'] );
 		$this->assertEquals( [ '920x250' ], $data['required_sizes'] );
 		$this->assertEquals( [ 'global_below_header' ], $data['placements'] );
-		$this->assertEquals( 'Ad &#8211; Below Header', get_the_title( $data['id'] ) );
 	}
 
 	/**
