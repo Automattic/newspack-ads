a4dd04121948d0c58c0a92d283d91856cec6023a
Miguel Peixe <miguel.peixe@a8c.com>
2023-06-19 10:24:53 -0300

    fix(gam): update api version

--- includes/providers/gam/api/class-orders.php
+++ includes/providers/gam/api/class-orders.php
@@ -9,11 +9,11 @@ namespace Newspack_Ads\Providers\GAM\Api;
 
 use Newspack_Ads\Providers\GAM\Api;
 use Newspack_Ads\Providers\GAM\Api\Api_Object;
-use Google\AdsApi\AdManager\Util\v202208\StatementBuilder;
-use Google\AdsApi\AdManager\v202208\ServiceFactory;
-use Google\AdsApi\AdManager\v202208\Order;
-use Google\AdsApi\AdManager\v202208\ArchiveOrders as ArchiveOrdersAction;
-use Google\AdsApi\AdManager\v202208\ApiException;
+use Google\AdsApi\AdManager\Util\v202305\StatementBuilder;
+use Google\AdsApi\AdManager\v202305\ServiceFactory;
+use Google\AdsApi\AdManager\v202305\Order;
+use Google\AdsApi\AdManager\v202305\ArchiveOrders as ArchiveOrdersAction;
+use Google\AdsApi\AdManager\v202305\ApiException;
 
 /**
  * Newspack Ads GAM Orders
