3eb0c309c1cc5fdef2353f0ab1a4016134ff5760
dkoo <derrick.koo@automattic.com>
2024-04-19 15:29:59 -0600

    fix: missing function arg

--- includes/providers/gam/class-gam-model.php
+++ includes/providers/gam/class-gam-model.php
@@ -1094,7 +1094,7 @@ final class GAM_Model {
 		if ( \is_user_logged_in() && method_exists( 'Newspack\Reader_Activation', 'is_user_reader' ) && \Newspack\Reader_Activation::is_user_reader( \wp_get_current_user() ) ) {
 			$targeting['reader_status'][] = 'logged_in'; // The currently logged-in user is a reader.
 			if ( method_exists( 'Newspack\Reader_Data', 'get_data' ) ) {
-				$reader_data = \Newspack\Reader_Data::get_data();
+				$reader_data = \Newspack\Reader_Data::get_data( get_current_user_id() );
 
 				// If reader has donated.
 				if ( ! empty( $reader_data['is_donor'] ) ) {
