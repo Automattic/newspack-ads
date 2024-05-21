2424cf3a875b726860e81b1f03824877577a88ca
Adam Cassis <adam.cassis@automattic.com>
2024-05-21 09:13:06 +0200

    trying to update eslint, no thank you

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-require( '@rushstack/eslint-patch/modern-module-resolution' );
-
-module.exports = {
-	extends: [ './node_modules/newspack-scripts/config/eslintrc.js' ],
-};
---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import config from 'newspack-scripts/config/eslintrc.js';
+
+export default [
+	config,
+	{
+		files: [ '**/*.js', '**/*.jsx' ],
+	},
+];
