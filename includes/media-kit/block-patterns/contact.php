3e3a06ebdfb48932eb3cc226c3922278f70e46ac
Thomas Guillot <thomasguillot@users.noreply.github.com>
2025-09-02 15:49:16 +0100

    fix(media-kit): add correct border-radius to contact patterns (#990)

--- includes/media-kit/block-patterns/contact.php
+++ includes/media-kit/block-patterns/contact.php
@@ -1,8 +1,8 @@
 <?php
 // phpcs:ignoreFile
 ?>
-<!-- wp:group {"metadata":{"name":"<?php echo esc_html__( 'Contact', 'newspack-ads' ); ?>"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"border":{"radius":"6px","width":"1px"}},"borderColor":"base-3","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-border-color has-base-3-border-color" style="border-width:1px;border-radius:6px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center"} -->
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html__( 'Contact', 'newspack-ads' ); ?>"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}},"border":{"radius":"0.5rem","width":"1px"}},"borderColor":"base-3","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-border-color has-base-3-border-color" style="border-width:1px;border-radius:0.5rem;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center"} -->
 <h2 class="wp-block-heading has-text-align-center"><?php echo esc_html__( 'Get started today', 'newspack-ads' ); ?></h2>
 <!-- /wp:heading -->
 
