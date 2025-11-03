3dcb58befd5db60d2a343181d2df9a85f2bd8657
Derrick Koo <dkoo@users.noreply.github.com>
2025-11-03 11:18:21 -0700

    chore: update dependencies (oct 2025) (#999)
    * chore: update dependencies (oct 2025)
    
    * chore: fix JS to satisfy linters
    
    * chore: remove ignoreFiles from .stylelintrc.js (moved to newspack-scripts)

--- src/frontend/side-rail-placements.js
+++ src/frontend/side-rail-placements.js
@@ -32,55 +32,52 @@ window.googletag = window.googletag || { cmd: [] };
  *
  * @return {void}
  */
-function initPlacement(selector, side, elements) {
-	const main = document.querySelector(mainElement);
-	if (!main) {
+function initPlacement( selector, side, elements ) {
+	const main = document.querySelector( mainElement );
+	if ( ! main ) {
 		return;
 	}
 
-	const element = document.querySelector(selector);
-	if (!element) {
+	const element = document.querySelector( selector );
+	if ( ! element ) {
 		return;
 	}
 	element.style.right = 'auto';
 
-	const ad = element.querySelector('div');
-	if (!ad) {
+	const ad = element.querySelector( 'div' );
+	if ( ! ad ) {
 		return;
 	}
 
-	ad.classList.add('ad-slot');
+	ad.classList.add( 'ad-slot' );
 
-	const header = document.querySelector('#masthead');
+	const header = document.querySelector( '#masthead' );
 
 	// Prepend a reference div to the element.
-	const refDiv = document.createElement('div');
-	refDiv.style.width = (ad._size ? ad._size[0] : ad.offsetWidth) + 'px';
-	refDiv.style.height = (ad._size ? ad._size[1] : ad.offsetHeight) + 'px';
+	const refDiv = document.createElement( 'div' );
+	refDiv.style.width = ( ad._size ? ad._size[ 0 ] : ad.offsetWidth ) + 'px';
+	refDiv.style.height = ( ad._size ? ad._size[ 1 ] : ad.offsetHeight ) + 'px';
 	refDiv.style.position = 'absolute';
 	refDiv.style.pointerEvents = 'none';
-	element.prepend(refDiv);
+	element.prepend( refDiv );
 
 	const hideAd = () => {
-		ad.classList.add('ad-hidden');
-		ad.classList.remove('ad-visible');
+		ad.classList.add( 'ad-hidden' );
+		ad.classList.remove( 'ad-visible' );
 	};
 	const showAd = () => {
-		ad.classList.remove('ad-hidden');
-		ad.classList.add('ad-visible');
-		ad.style.removeProperty('display');
+		ad.classList.remove( 'ad-hidden' );
+		ad.classList.add( 'ad-visible' );
+		ad.style.removeProperty( 'display' );
 	};
 
 	const handleCollision = () => {
-		if (
-			ad.style.width &&
-			parseInt(ad.style.width.replace('px', '')) > element.offsetWidth
-		) {
+		if ( ad.style.width && parseInt( ad.style.width.replace( 'px', '' ) ) > element.offsetWidth ) {
 			hideAd();
 			return;
 		}
 
-		if (elementCollides(refDiv, elements)) {
+		if ( elementCollides( refDiv, elements ) ) {
 			hideAd();
 		} else {
 			showAd();
@@ -88,37 +85,35 @@ function initPlacement(selector, side, elements) {
 	};
 
 	const updateDimensions = () => {
-		if (hasStickyHeader()) {
+		if ( hasStickyHeader() ) {
 			const headerRect = header.getBoundingClientRect();
-			element.style.top = `${headerRect.bottom}px`;
+			element.style.top = `${ headerRect.bottom }px`;
 		}
 
 		const mainRect = main.getBoundingClientRect();
 		let newWidth = 0;
-		if (side === 'left') {
+		if ( side === 'left' ) {
 			element.style.left = '0';
 			newWidth = mainRect.left;
 		} else {
-			element.style.left = `${mainRect.right}px`;
+			element.style.left = `${ mainRect.right }px`;
 			newWidth = window.innerWidth - mainRect.right;
 		}
 
-		element.style.width = `${newWidth}px`;
+		element.style.width = `${ newWidth }px`;
 	};
 
 	const handleStickyAd = () => {
-		const stickyAd = document.querySelector('.newspack_global_ad.sticky');
-		const stickyAdClose = document.querySelector(
-			'.newspack_sticky_ad__close'
-		);
-		if (stickyAd) {
-			element.style.bottom = `${stickyAd.offsetHeight}px`;
+		const stickyAd = document.querySelector( '.newspack_global_ad.sticky' );
+		const stickyAdClose = document.querySelector( '.newspack_sticky_ad__close' );
+		if ( stickyAd ) {
+			element.style.bottom = `${ stickyAd.offsetHeight }px`;
 		}
-		if (stickyAdClose) {
-			stickyAdClose.addEventListener('click', () => {
-				element.style.removeProperty('bottom');
+		if ( stickyAdClose ) {
+			stickyAdClose.addEventListener( 'click', () => {
+				element.style.removeProperty( 'bottom' );
 				handlePlacement();
-			});
+			} );
 		}
 	};
 
@@ -129,26 +124,24 @@ function initPlacement(selector, side, elements) {
 	};
 	handlePlacement();
 
-	window.addEventListener('scroll', debounce(handlePlacement, 50));
-	window.addEventListener('resize', debounce(handlePlacement, 200));
+	window.addEventListener( 'scroll', debounce( handlePlacement, 50 ) );
+	window.addEventListener( 'resize', debounce( handlePlacement, 200 ) );
 
-	window.googletag.cmd.push(function () {
-		window.googletag
-			.pubads()
-			.addEventListener('slotRenderEnded', function (event) {
-				if (ad.id !== event.slot.getSlotElementId()) {
-					return;
-				}
-				refDiv.style.width = event.size[0] + 'px';
-				refDiv.style.height = event.size[1] + 'px';
-				handlePlacement();
-			});
-	});
+	window.googletag.cmd.push( function () {
+		window.googletag.pubads().addEventListener( 'slotRenderEnded', function ( event ) {
+			if ( ad.id !== event.slot.getSlotElementId() ) {
+				return;
+			}
+			refDiv.style.width = event.size[ 0 ] + 'px';
+			refDiv.style.height = event.size[ 1 ] + 'px';
+			handlePlacement();
+		} );
+	} );
 }
 
-domReady(() => {
-	const elements = document.querySelectorAll(collisionElements.join(','));
+domReady( () => {
+	const elements = document.querySelectorAll( collisionElements.join( ',' ) );
 
-	initPlacement('.newspack_global_ad.left_side_rail', 'left', elements);
-	initPlacement('.newspack_global_ad.right_side_rail', 'right', elements);
-});
+	initPlacement( '.newspack_global_ad.left_side_rail', 'left', elements );
+	initPlacement( '.newspack_global_ad.right_side_rail', 'right', elements );
+} );
